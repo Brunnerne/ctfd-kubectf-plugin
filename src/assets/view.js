56576455a893593aa470187a00be1f07151012d4
The0Mikkel <mikkel.alb@gmail.com>
2024-10-26 00:16:28 +0200

    Move to use of vanilla javascript and fix minor issues with retrieval of data

--- src/assets/view.js
+++ src/assets/view.js
@@ -1,7 +1,6 @@
 CTFd._internal.challenge.data = undefined
 
-CTFd._internal.challenge.renderer = CTFd.lib.markdown();
-
+// CTFd._internal.challenge.renderer = CTFd.lib.markdown();
 
 CTFd._internal.challenge.preRender = function () { }
 
@@ -9,13 +8,11 @@ CTFd._internal.challenge.render = function (markdown) {
     return CTFd._internal.challenge.renderer.render(markdown)
 }
 
-
 CTFd._internal.challenge.postRender = function () { }
 
-
 CTFd._internal.challenge.submit = function (preview) {
-    var challenge_id = parseInt(CTFd.lib.$('#challenge-id').val())
-    var submission = CTFd.lib.$('#challenge-input').val()
+    var challenge_id = parseInt(document.querySelector('#challenge-id').value)
+    var submission = document.querySelector('#challenge-input').value
 
     var body = {
         'challenge_id': challenge_id,
@@ -47,23 +44,37 @@ function toggleLoading(btn) {
 }
 
 function resetAlert() {
-    let alert = $(".deployment-actions > .alert").first();
-    alert.empty();
-    alert.removeClass("alert-danger");
+    let alert = document.querySelector(".deployment-actions > .alert");
+    alert.innerHTML = '';
+    alert.classList.remove("alert-danger");
     return alert;
 }
 
+function handleErrorResponse(response, alert, btn) {
+    if (response.status === 429) {
+        alert.append("Slow down! You are too fast. Try again in 1 minute.");
+        alert.classList.add("alert-danger");
+    } else if (response.status >= 400 && response.status != 404) {
+        alert.append(response.error || response.message || "An error occurred while processing the challenge.");
+        alert.classList.add("alert-danger");
+    }
+
+    if (btn) {
+        toggleLoading(btn);
+    }
+}
+
 function toggleChallengeCreate() {
-    let btn = $(".create-chal").first();
-    btn.toggleClass('d-none');
+    let btn = document.querySelector(".create-chal");
+    btn.classList.toggle('d-none');
 }
 
 function toggleChallengeUpdate() {
-    let btn = $(".extend-chal").first();
-    btn.toggleClass('d-none');
+    let btn = document.querySelector(".extend-chal");
+    btn.classList.toggle('d-none');
 
-    btn = $(".terminate-chal").first();
-    btn.toggleClass('d-none');
+    btn = document.querySelector(".terminate-chal");
+    btn.classList.toggle('d-none');
 }
 
 function calculateExpiry(date) {
@@ -73,6 +84,14 @@ function calculateExpiry(date) {
 }
 
 function createChallengeLinkElement(data, parent) {
+    if (!data.deployment) {
+        let alert = resetAlert();
+        alert.append("Challenge not started");
+        toggleChallengeCreate();
+        toggleChallengeUpdate();
+        return;
+    }
+
     let expiry = calculateExpiry(new Date(data.deployment.expires));
 
     if (expiry > 0) {
@@ -96,42 +115,55 @@ function createChallengeLinkElement(data, parent) {
         }
 
         // Add admin bot link if challenge is tagged with bot
-        const chalTag = CTFd.lib.$(".challenge-tag").last().text();
-        if (chalTag.startsWith("#")) {
-            parent.append(document.createElement("br"))
-            let link = document.createElement('a');
-            const subdomains = new URL(`https://${data.deployment.host}`).hostname.split('.')
-            const unique = `${subdomains.shift(1)}-${chalTag.substring(1)}`;
-            const host = `https://${unique}.${subdomains.join('.')}`
-            link.href =  host;
-            link.target = '_blank'
-            link.textContent = host;
-            parent.append(link);
+        try {
+            const chalTag = document.querySelector(".challenge-tag").textContent;
+            if (chalTag.startsWith("#")) {
+                parent.append(document.createElement("br"))
+                let link = document.createElement('a');
+                const subdomains = new URL(`https://${data.deployment.host}`).hostname.split('.')
+                const unique = `${subdomains.shift(1)}-${chalTag.substring(1)}`;
+                const host = `https://${unique}.${subdomains.join('.')}`
+                link.href = host;
+                link.target = '_blank'
+                link.textContent = host;
+                parent.append(link);
+            }
+        } catch (error) {
+
         }
-        
-    } 
+
+    }
 }
 
 function awaitChallengeReady(data) {
-    
+
 }
 
 function getDeployment(deployment) {
     let alert = resetAlert();
 
-    $.ajax({
-        type: "GET",
-        url: "api/kube_ctf/" + deployment,
-        success: function(data) {
+    fetch("api/kube_ctf/" + deployment)
+        .then(response => {
+            if (response.status >= 400 && response.status != 404) {
+                handleErrorResponse(response, alert);
+                return;
+            }
+
+            return response.json()
+        })
+        .then(data => {
+            if (!data) {
+                return;
+            }
 
             createChallengeLinkElement(data, alert);
             toggleChallengeUpdate();
-        },
-        error: function(error) {
+        })
+        .catch(error => {
+            console.error(error);
             alert.append("Challenge not started")
             toggleChallengeCreate();
-        }
-    }) 
+        });
 }
 
 function createDeployment(btn) {
@@ -139,24 +171,36 @@ function createDeployment(btn) {
     toggleLoading(btn);
     let alert = resetAlert();
 
-    // Can't use the nice format cause need to put content-type header in
-    $.ajax({
-        type: "POST",
-        url: "api/kube_ctf/" + deployment,
-        data: JSON.stringify({action: "create"}),
-        contentType: "application/json",
-        success: function(data) {
-	    createChallengeLinkElement(data, alert);
-	    toggleChallengeUpdate();
-	    toggleChallengeCreate();
-	    toggleLoading(btn);
+    fetch("api/kube_ctf/" + deployment, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
         },
-        error: function(error) {
-            alert.append(error.responseJSON.error || error.responseJSON.message)
-            alert.addClass("alert-danger")
+        body: JSON.stringify({ action: "create" })
+    })
+        .then(response => {
+            if (response.status >= 400 && response.status != 404) {
+                handleErrorResponse(response, alert, btn);
+                return
+            }
+
+            return response.json()
+        })
+        .then(data => {
+            if (!data) {
+                return;
+            }
+
+            createChallengeLinkElement(data, alert);
+            toggleChallengeUpdate();
+            toggleChallengeCreate();
             toggleLoading(btn);
-        }
-    }) 
+        })
+        .catch(error => {
+            alert.append(error.responseJSON?.error || error.responseJSON?.message || error.message);
+            alert.classList.add("alert-danger");
+            toggleLoading(btn);
+        });
 }
 
 function extendDeployment(btn) {
@@ -164,50 +208,68 @@ function extendDeployment(btn) {
     toggleLoading(btn);
     let alert = resetAlert();
 
-    // Can't use the nice format cause need to put content-type header in
-    $.ajax({
-        type: "POST",
-        url: "api/kube_ctf/" + deployment,
-        data: JSON.stringify({action: "extend"}),
-        contentType: "application/json",
-        success: function(data) {
+    fetch("api/kube_ctf/" + deployment, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({ action: "extend" })
+    })
+        .then(response => {
+            if (response.status >= 400 && response.status != 404) {
+                handleErrorResponse(response, alert, btn);
+                return;
+            }
+
+            return response.json()
+        })
+        .then(data => {
+            if (!data) {
+                return;
+            }
+
             createChallengeLinkElement(data, alert)
             toggleLoading(btn);
-        },
-        error: function(error) {
-            alert.append(error.responseJSON.error || error.responseJSON.message)
-            alert.addClass("alert-danger")
+        })
+        .catch(error => {
+            alert.append(error.responseJSON?.error || error.responseJSON?.message || error.message);
+            alert.classList.add("alert-danger");
             toggleLoading(btn);
-        }
-    })    
-
+        });
 }
-
-// function resetDeployment() {
-
-// }
 
 function terminateDeployment(btn) {
     let deployment = btn.dataset.deployment;
     toggleLoading(btn);
     let alert = resetAlert();
 
-    // Can't use the nice format cause need to put content-type header in
-    $.ajax({
-        type: "POST",
-        url: "api/kube_ctf/" + deployment,
-        data: JSON.stringify({action: "terminate"}),
-        contentType: "application/json",
-        success: function(data) {
-            alert.append("Challenge Terminated.")
-            toggleChallengeCreate();
-            toggleChallengeUpdate();
-            toggleLoading(btn);
+    fetch("api/kube_ctf/" + deployment, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
         },
-        error: function(error) {
-            alert.append(error.responseJSON.error || error.responseJSON.message)
-            alert.addClass("alert-danger")
-            toggleLoading(btn);
-        }
+        body: JSON.stringify({ action: "terminate" })
     })
+        .then(response => {
+            if (response.status === 200 || response.status === 204) {
+                alert.append("Challenge Terminated.")
+                toggleChallengeCreate();
+                toggleChallengeUpdate();
+                toggleLoading(btn);
+                return;
+            }
+
+            handleErrorResponse(response, alert, btn);
+        })
+        .catch(error => {
+            alert.append(error.responseJSON?.error || error.responseJSON?.message || error.message || "An error occurred while terminating the challenge.");
+            alert.classList.add("alert-danger");
+            toggleLoading(btn);
+        });
 }
+
+// Start app, by first getting the deployment name
+setTimeout(() => {
+    let deployment = document.getElementById("deployment-name").value;
+    getDeployment(deployment);
+}, 200);
--- src/routes.py
+++ src/routes.py
@@ -1,4 +1,5 @@
 import re
+import os
 from urllib.parse import urlparse
 
 import requests
@@ -11,10 +12,11 @@ from CTFd.utils.user import get_current_user
 from CTFd.utils.decorators import during_ctf_time_only
 from flask import Blueprint, render_template, request
 
+
 from .config import CHALLENGE_REGEX, CONFIG_HOST, CONFIG_SECRET
 from .errors import (ConfigurationError, InvalidRequest, NotAuthenticatedError,
                      UnknownError, ValidationError)
-from .lib import error, get_deployment, create_deployment, extend_deployment, terminate_deployment
+from .lib import error, get_deployment, get_deployments, create_deployment, extend_deployment, terminate_deployment
 
 blueprint = Blueprint(
     'kube_ctf',
@@ -22,10 +24,62 @@ blueprint = Blueprint(
     template_folder='templates',
     static_folder='assets')
 
-
 def register_app(app):
     app.register_blueprint(blueprint)
+    
+def get_config_host(default=None):
+    # If environment variable is set, use that
+    host = os.getenv('KUBECTF_HOST')
+    if host is not None and host != '':
+        return host
+    
+    host = get_config(CONFIG_HOST)
+    if not host:
+        return default
+    return host
 
+def get_config_secret(default=None):
+    # If environment variable is set, use that
+    secret = os.getenv('KUBECTF_SECRET')
+    if secret is not None and secret != '':
+        return secret
+    
+    secret = get_config(CONFIG_SECRET)
+    if not secret:
+        return default
+    return secret
+
+@blueprint.route('/api/kube_ctf/', methods=['GET'])
+@bypass_csrf_protection
+@during_ctf_time_only
+@ratelimit(method='GET', limit=15, interval=60, key_prefix='rl_kctfget')
+@ratelimit(method='POST', limit=4, interval=60, key_prefix='rl_kctfpost')
+def get_challenges():
+    session = get_current_user()
+
+    if not session or not session.id:
+        return error(NotAuthenticatedError)
+    
+    owner_id = session.id
+    if get_config('user_mode') == TEAMS_MODE:
+        if (not session.team_id):
+            return error(NotAuthenticatedError)
+        owner_id = session.team_id
+
+
+    host = get_config_host()
+    secret = get_config_secret()
+    if not host or not secret:
+        return error(ConfigurationError)
+
+    try:
+        if request.method == 'GET':
+            return get_deployments(host, secret, owner_id, False)
+        else:
+            return error(InvalidRequest)
+    except Exception as e:
+        log('kube_ctf', 'unknown error: {error}', error=e)
+        return error(UnknownError)
 
 @blueprint.route('/api/kube_ctf/<challenge>', methods=['GET', 'POST'])
 @bypass_csrf_protection
@@ -35,11 +89,13 @@ def register_app(app):
 def get_challenge(challenge):
     session = get_current_user()
 
-    if not session or not session.id or not session.team_id:
+    if not session or not session.id:
         return error(NotAuthenticatedError)
     
     owner_id = session.id
     if get_config('user_mode') == TEAMS_MODE:
+        if (not session.team_id):
+            return error(NotAuthenticatedError)
         owner_id = session.team_id
 
 
@@ -47,8 +103,8 @@ def get_challenge(challenge):
         return error(ValidationError)
     challenge = challenge.lower()
 
-    host = get_config(CONFIG_HOST)
-    secret = get_config(CONFIG_SECRET)
+    host = get_config_host()
+    secret = get_config_secret()
     if not host or not secret:
         return error(ConfigurationError)
 
@@ -90,6 +146,6 @@ def get_config_page():
 
     return render_template(
         'admin.html',
-        host=get_config(CONFIG_HOST, ''),
-        secret=get_config(CONFIG_SECRET, ''),
+        host=get_config_host(''),
+        secret=get_config_secret(''),
         alert=alert)
