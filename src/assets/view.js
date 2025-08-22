f4ccc05624d6d8a0bd006b9b24c69fb9f661d4ab
Mikkel Albrechtsem <me@themikkel.dk>
2025-08-22 15:34:55 +0200

    Fix error message for maximum active challenges in deployment response

--- src/assets/view.js
+++ src/assets/view.js
@@ -55,7 +55,7 @@ function handleErrorResponse(response, alert, btn) {
 		alert.append("Slow down! You are too fast. Try again in 1 minute.");
 		alert.classList.add("alert-danger");
 	} else if (response.status != 403) {
-		alert.append("Maximum active deployments reached. Please terminate some challenges.");
+		alert.append("Maximum active challenges reached. Please terminate some challenges.");
 		alert.classList.add("alert-danger");
 	} else if (response.status >= 400 && response.status != 404) {
 		alert.append(response.error || response.message || "An error occurred while processing the challenge.");
